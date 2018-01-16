b134a40029c87d680d76b9bf2e52c6c3686ac150
PlawanTH <plawan.q@gmail.com>
2018-01-16 17:00:57 +0700

    Fix multiple time submit button

--- App/Views/register.php
+++ App/Views/register.php
@@ -12,13 +12,20 @@
 
 		<?php else: ?>
 
-			<form method="post" action="/register/submit" >
+			<form method="post" action="/register/submit" onsubmit="return checkForm(this)">
 				Name: <input type="text" name="name" /> <br/>
 				Username: <input type="text" name="username" /> <br/>
 				Password: <input type="password" name="password" /> <br/>
 				E-mail: <input type="text" name="email" /> <br/>
 				<input type="submit" name="submit" />
 			</form>
+
+			<script>
+				function checkForm(form) {
+					form.submit.disabled = true;
+					return true;
+				}
+			</script>
 
 		<?php endif; ?>
 
